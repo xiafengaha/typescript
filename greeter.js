d4a5c13b3b89f2187bbbb4a1ce7bc88653a3e081
xiafeng <774147110@qq.com>
2021-03-11 10:40:48 +0800

    项目初始化

---
+++ greeter.js
@@ -0,0 +1,5 @@
+function greeter(person) {
+    return "Hello," + person;
+}
+var user = "xiaxiaofeng";
+document.body.innerHTML = greeter(user);
---
+++ greeter.ts
@@ -0,0 +1,5 @@
+function greeter(person) {
+    return "Hello," + person
+}
+let user = "xiaxiaofeng";
+document.body.innerHTML = greeter(user);
